3311a7cb774b1158b761fea7497413d03618d566
Ponco <poncopurnomo18@gmail.com>
2017-11-03 23:03:18 +0700

    UTS

---
+++ KasirSuperResto/app/src/main/java/com/example/user/kasirsuperresto/SettingHarga.java
@@ -0,0 +1,13 @@
+package com.example.user.kasirsuperresto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingHarga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_harga);
+    }
+}
---
+++ KasirSuperResto/app/src/main/java/com/example/user/kasirsuperresto/StrukPembelian.java
@@ -0,0 +1,13 @@
+package com.example.user.kasirsuperresto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StrukPembelian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_struk_pembelian);
+    }
+}
